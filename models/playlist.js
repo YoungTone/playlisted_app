aafa2aa4e5303e936a8d9823d5fba4c7d244968d
tone <anthonyschwartzcode@gmail.com>
2015-09-30 08:52:56 -0700

    Added Playlist Modle

---
+++ models/playlist.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+
+var playlistSchema = new mongoose.Schema({
+    location: String,
+
+    // Child Association
+    songs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Song"
+
+    }],
+
+    // Parent Association
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+playlistSchema.pre('remove', function(callback) {
+    Song.remove({
+        playlist: this._id
+    }).exec();
+    callback();
+});
+
+var Playlist = mongoose.model('Playlist', playlistSchema);
+
+module.exports = Playlist;
